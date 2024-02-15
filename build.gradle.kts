5de4247576aec48ca31033f0c509bf5cf458a3d8
StarStudio <charlottewiltshire27@outlook.com>
2024-02-15 20:47:54 +0200

    Dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,17 +26,32 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    // Spring
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.springframework.security:spring-security-web:6.2.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.2")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:3.2.2")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.2.2")
+
+    // Other
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+
+    // JWT tokens
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.3")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
+
+    // Database
     runtimeOnly("org.postgresql:postgresql")
-    annotationProcessor("org.projectlombok:lombok")
+
+    // Test
+    testImplementation("org.springframework.security:spring-security-test:6.2.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
